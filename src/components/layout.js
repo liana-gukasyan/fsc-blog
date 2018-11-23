056f8dd39ef75ca0bb068e0596ba871aa643bbd4
Liana <lia.gukasyan@gmail.com>
2018-11-23 20:08:37 +0300

    Add proper meta information to the head

--- src/components/layout.js
+++ src/components/layout.js
@@ -12,10 +12,29 @@ import './layout.css'
 const Layout = ({ children }) => (
   <div>
     <Helmet
-      title="The Gray Web Blog"
+      title='First Step Coding | Bootcamp Prep'
       meta={[
-        { name: 'description', content: 'Sample' },
-        { name: 'keywords', content: 'sample, something' },
+        { charset: 'UTF-8' },
+        { name: 'viewport', content: 'width=device-width, initial-scale=1' },
+        { property: 'og:site_name', content: 'First Step Coding' },
+        { property: 'og:title', content: 'First Step Coding | Bootcamp Prep' },
+        { property: 'og:url', content: 'https://blog.firststepcoding.com/' },
+        { property: 'og:type', content: 'website' },
+        { property: 'og:description', content: 'Part-time beginner-friendly coding courses in small groups with 8-10 students. Level up in your career, or begin a new adventure.' },
+        { property: 'og:image', content: 'http://static1.squarespace.com/static/57c22a12d1758e5dd82ca7e6/t/581cb41f59cc6833a5e1ea95/1478276127771/icon-only.png?format=1500w' },
+        { property: 'og:image:width', content: '362' },
+        { property: 'og:image:height', content: '251' },
+        { itemprop: 'name', content: 'First Step Coding | Bootcamp Prep' },
+        { itemprop: 'url', content: 'https://blog.firststepcoding.com/' },
+        { itemprop: 'description', content: 'Part-time beginner-friendly coding courses in small groups with 8-10 students. Level up in your career, or begin a new adventure.' },
+        { itemprop: 'thumbnailUrl', content: 'http://static1.squarespace.com/static/57c22a12d1758e5dd82ca7e6/t/581cb41f59cc6833a5e1ea95/1478276127771/icon-only.png?format=1500w' },
+        { itemprop: 'image', content: 'http://static1.squarespace.com/static/57c22a12d1758e5dd82ca7e6/t/581cb41f59cc6833a5e1ea95/1478276127771/icon-only.png?format=1500w' },
+        { name: 'twitter:title', content: 'First Step Coding | Bootcamp Prep'},
+        { name: 'twitter:image', content: 'http://static1.squarespace.com/static/57c22a12d1758e5dd82ca7e6/t/581cb41f59cc6833a5e1ea95/1478276127771/icon-only.png?format=1500w'},
+        { name: 'twitter:url', content: 'https://blog.firststepcoding.com/'},
+        { name: 'twitter:card', content: 'summary'},
+        { name: 'twitter:description', content: 'Part-time beginner-friendly coding courses in small groups with 8-10 students. Level up in your career, or begin a new adventure.'},
+        { name: 'description', content: 'Part-time beginner-friendly coding courses in small groups with 8-10 students. Level up in your career, or begin a new adventure.'},
       ]}
     />
     <Header />
